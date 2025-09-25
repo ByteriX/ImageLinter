2555f67b49c287c3ec3ed75fc218485024b64bba
Sergey Balalaev <sbalalaev@Sergeys-Mac-mini.local>
2025-09-25 13:49:16 +0400

    Release 2.1.2 version with updating Swift script

--- ImageLinter.swift
+++ ImageLinter.swift
@@ -5,7 +5,7 @@ import AppKit
 
 /**
  ImageLinter.swift
- version 2.1.1
+ version 2.1.2
 
  Created by Sergey Balalaev on 23.09.22.
  Copyright (c) 2022-2025 ByteriX. All rights reserved.
@@ -1093,16 +1093,22 @@ while let imageFileName = imageFileEnumerator?.nextObject() as? String {
                 }
 
                 if settings.isCheckingPdfVector || settings.isCheckingSvgVector {
-                    if let string = try? String(contentsOfFile: imageFilePath, encoding: .ascii) {
-                        let range = NSRange(location: 0, length: string.count)
-                        if settings.isCheckingPdfVector, pdfRasterRegex?.firstMatch(in: string, options: [], range: range) != nil {
-                            printError(filePath: imageFilePath, message: "PDF File is not vector. Found for image '\(imageInfo.name)'")
-                        }
-                        if settings.isCheckingSvgVector, svgRasterRegex?.firstMatch(in: string, options: [], range: range) != nil {
-                            printError(filePath: imageFilePath, message: "SVG File is not vector. Found for image '\(imageInfo.name)'")
-                        }
+                    if !FileManager.default.isReadableFile(atPath: imageFilePath) {
+                        printError(filePath: imageFilePath, message: "Can not read file with path: '\(imageFilePath)'")
                     } else {
-                        printError(filePath: imageFilePath, message: "Can not parse Vector file. Found for image '\(imageInfo.name)'")
+                        do {
+                            let string = try String(contentsOfFile: imageFilePath, encoding: .isoLatin1)
+
+                            let range = NSRange(location: 0, length: string.count)
+                            if settings.isCheckingPdfVector, pdfRasterRegex?.firstMatch(in: string, options: [], range: range) != nil {
+                                printError(filePath: imageFilePath, message: "PDF File is not a pure vector. Found for image '\(imageInfo.name)'")
+                            }
+                            if settings.isCheckingSvgVector, svgRasterRegex?.firstMatch(in: string, options: [], range: range) != nil {
+                                printError(filePath: imageFilePath, message: "SVG File is not a pure vector. Found for image '\(imageInfo.name)'")
+                            }
+                        } catch let error {
+                            printError(filePath: imageFilePath, message: "Can not parse Vector file. Found for image '\(imageInfo.name)' with error: `\(error.localizedDescription)`")
+                        }
                     }
                 }
             } else if settings.rastorExtensions.contains(fileExtension) {
