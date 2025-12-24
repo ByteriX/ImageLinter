cbd7bcc5e3cca16db74bea460f217ad31c06f6ab
Sergey Balalaev <sof.bix@mail.ru>
2025-12-24 23:31:35 +0400

    complex test of filter

--- Tests/ImagelinterExecTests/ImageFilterParseTests.swift
+++ Tests/ImagelinterExecTests/ImageFilterParseTests.swift
@@ -30,5 +30,25 @@ struct ImageFilterParseTests {
         #expect(sizeCondition.size[0].comparison == comparison)
     }
 
-    
+    @Test
+    func complex() throws {
+        let pattern = "vector, >100kpx , <10Mb)"
+        let filter = try #require(ImageFilter(pattern))
+        #expect(filter.andConditions.count == 3)
+        let orCondition1 = try #require(filter.andConditions[0] as? ImageFilter.OrCondition)
+        let typeCondition = try #require(orCondition1.orConditions.first as? ImageFilter.ImageTypeCondition)
+        #expect(typeCondition.types.count == 1)
+        #expect(typeCondition.types.contains(.vector) == true)
+        let orCondition2 = try #require(filter.andConditions[1] as? ImageFilter.OrCondition)
+        let sizeCondition1 = try #require(orCondition2.orConditions.first as? ImageFilter.SizeCondition)
+        #expect(sizeCondition1.size[0].comparison == ImageFilter.Comparison.greater)
+        #expect(sizeCondition1.size[0].value == 100_000)
+        #expect(sizeCondition1.size[0].messure == .pixels)
+        let orCondition3 = try #require(filter.andConditions[2] as? ImageFilter.OrCondition)
+        let sizeCondition2 = try #require(orCondition3.orConditions.first as? ImageFilter.SizeCondition)
+        #expect(sizeCondition2.size[0].comparison == ImageFilter.Comparison.less)
+        #expect(sizeCondition2.size[0].value == 10 * 1024 * 1024)
+        #expect(sizeCondition2.size[0].messure == .bytes)
+    }
+
 }
