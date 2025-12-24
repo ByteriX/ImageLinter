8519cd479119a11c9ee29d316aef54cfdd1ae747
Sergey Balalaev <sof.bix@mail.ru>
2025-12-24 21:55:54 +0400

    fixed issue with parsing checkingNameTypes

--- Tests/ImagelinterExecTests/SettingsTests.swift
+++ Tests/ImagelinterExecTests/SettingsTests.swift
@@ -9,8 +9,8 @@ import Testing
 import ImagelinterExec
 import Foundation
 
+@Suite("Settings parser tests")
 struct SettingsTests {
-
 
     @Test
     func testSimplePath() {
@@ -36,6 +36,18 @@ struct SettingsTests {
             """)
         #expect(settings.relativeImagesPaths == ["1/", "2/"])
         #expect(settings.relativeSourcePaths == ["3/", "4/"])
+    }
+
+    @Test
+    func testSimpleUsingType() throws {
+        let settings = Settings("""
+            usingTypes:
+              - case: uiKit
+            """)
+        #expect(settings.usingTypes.count == 1)
+
+        let usingType = try #require(settings.usingTypes.first)
+        #expect(usingType == .uiKit)
     }
 
     @Test
@@ -106,6 +118,163 @@ struct SettingsTests {
     }
 
     @Test
+    func testSimpleCheckingNameTypes() throws {
+        let settings = Settings("""
+            checkingNameTypes:
+              - case: camelCase
+            """)
+        #expect(settings.checkingNameTypes.count == 1)
+
+        let checkingNameTypes = try #require(settings.checkingNameTypes.first)
+        if case let .camelCase(message: message, filter: filter) = checkingNameTypes {
+            #expect(message == "Camel case support only")
+            #expect(filter == nil)
+        } else {
+            Issue.record("Wrong type: \(checkingNameTypes)")
+        }
+    }
+
+    @Test
+    func testCheckingNameTypesWithMessage() throws {
+        let settings = Settings("""
+            checkingNameTypes:
+              - case: firstUpperCase
+                message: Test
+            """)
+        #expect(settings.checkingNameTypes.count == 1)
+
+        let checkingNameTypes = try #require(settings.checkingNameTypes.first)
+        if case let .firstUpperCase(message: message, filter: filter) = checkingNameTypes {
+            #expect(message == "Test")
+            #expect(filter == nil)
+        } else {
+            Issue.record("Wrong type: \(checkingNameTypes)")
+        }
+    }
+
+    @Test
+    func testCheckingNameTypesWithFilter() throws {
+        let settings = Settings("""
+            checkingNameTypes:
+              - case: camelCase
+                filter: vector, >1kb
+            """)
+        #expect(settings.checkingNameTypes.count == 1)
+
+        let checkingNameTypes = try #require(settings.checkingNameTypes.first)
+        if case let .camelCase(message: message, filter: filter) = checkingNameTypes {
+            #expect(message == "Camel case support only")
+            #expect(filter != nil)
+        } else {
+            Issue.record("Wrong type: \(checkingNameTypes)")
+        }
+    }
+
+    @Test
+    func testCheckingNameTypesWithMessageAndFilter() throws {
+        let settings = Settings("""
+            checkingNameTypes:
+              - case: firstUpperCase
+                message: Test
+                filter: vector, > 0.05Kpx
+            """)
+        #expect(settings.checkingNameTypes.count == 1)
+
+        let checkingNameTypes = try #require(settings.checkingNameTypes.first)
+        if case let .firstUpperCase(message: message, filter: filter) = checkingNameTypes {
+            #expect(message == "Test")
+            #expect(filter != nil)
+        } else {
+            Issue.record("Wrong type: \(checkingNameTypes)")
+        }
+    }
+
+    @Test
+    func testManyCheckingNameTypes() throws {
+        let settings = Settings("""
+                        checkingNameTypes:
+                          - case: camelCase
+                            filter: vector, >1kb
+                          - case: firstUpperCase
+                            message: First case should be uppered for vector images with 50px and more
+                            filter: vector, > 0.05Kpx
+                          - case: custom
+                            pattern: .*
+            """)
+        #expect(settings.checkingNameTypes.count == 3)
+        let checkingNameType1 = settings.checkingNameTypes[0]
+        if case let .camelCase(message: message, filter: filter) = checkingNameType1 {
+            #expect(filter != nil)
+        } else {
+            Issue.record("Wrong type: \(checkingNameType1)")
+        }
+        let checkingNameType2 = settings.checkingNameTypes[1]
+        if case let .firstUpperCase(message: message, filter: filter) = checkingNameType2 {
+            #expect(message == "First case should be uppered for vector images with 50px and more")
+            #expect(filter != nil)
+        } else {
+            Issue.record("Wrong type: \(checkingNameType2)")
+        }
+        let checkingNameType3 = settings.checkingNameTypes[2]
+        if case let .custom(pattern: pattern, message: message, filter: filter) = checkingNameType3 {
+            #expect(pattern == ".*")
+            #expect(filter == nil)
+        } else {
+            Issue.record("Wrong type: \(checkingNameType3)")
+        }
+    }
+
+    @Test
+    func testCustomCheckingNameTypes() throws {
+        let settings = Settings("""
+                        checkingNameTypes:
+                          - case: custom
+                          - case: custom
+                          - case: custom
+                            pattern: .*1
+                            filter: vector, > 0.01Kpx
+                          - case: custom
+                            filter: vector, > 0.005Kpx
+                          - case: custom
+                            pattern: .*2
+                            message: Test2
+                          - case: custom
+                            pattern: .*3
+                            message: Test3
+                            filter: vector, > 0.02Kpx
+                          - case: custom
+                          - case: custom
+                            message: Test Fail
+                            filter: vector, > 0.1Kpx
+                          - case: custom
+                        """)
+        #expect(settings.checkingNameTypes.count == 3)
+        let checkingNameType1 = settings.checkingNameTypes[0]
+        if case let .custom(pattern: pattern, message: message, filter: filter) = checkingNameType1 {
+            #expect(pattern == ".*1")
+            #expect(filter != nil)
+        } else {
+            Issue.record("Wrong type: \(checkingNameType1)")
+        }
+        let checkingNameType2 = settings.checkingNameTypes[1]
+        if case let .custom(pattern: pattern, message: message, filter: filter) = checkingNameType2 {
+            #expect(pattern == ".*2")
+            #expect(filter == nil)
+            #expect(message == "Test2")
+        } else {
+            Issue.record("Wrong type: \(checkingNameType2)")
+        }
+        let checkingNameType3 = settings.checkingNameTypes[2]
+        if case let .custom(pattern: pattern, message: message, filter: filter) = checkingNameType3 {
+            #expect(pattern == ".*3")
+            #expect(filter != nil)
+            #expect(message == "Test3")
+        } else {
+            Issue.record("Wrong type: \(checkingNameType3)")
+        }
+    }
+
+    @Test
     func testAllParse() {
         let settings = Settings("""
             isEnabled: true
@@ -126,7 +295,7 @@ struct SettingsTests {
               - case: custom
                 pattern: "(.*)".image
                 isSwiftGen: true
-                - case: custom
+              - case: custom
                 pattern: "(.*)".name
             checkingNameTypes:
               - case: camelCase
@@ -135,22 +304,26 @@ struct SettingsTests {
                 message: First case should be uppered for vector images with 50px and more
                 filter: vector, > 0.05Kpx
             ignoredUnusedImages:
-              - temp
+              - temp1
             ignoredUndefinedImages:
-              - temp
+              - temp2
             rastorExtensions:
               - png
               - jpg
+              - tiff
               - jpeg
             vectorExtensions:
               - pdf
+              - vector
               - svg
             sourcesExtensions:
               - swift
               - mm
+              - c
             resourcesExtensions:
               - storyboard
               - xib
+              - swift
             isAllFilesErrorShowing: false
             maxVectorFileSize: 30000
             maxVectorImageSize:
@@ -178,7 +351,14 @@ struct SettingsTests {
         #expect(settings.isEnabled == true)
         #expect(settings.relativeSourcePaths == ["/", "/Sources1/", "/Sources2/"])
         #expect(settings.relativeImagesPaths == ["/Sources/Images/Resources/Error/"])
-
+        #expect(settings.usingTypes.count == 7)
+        #expect(settings.checkingNameTypes.count == 2)
+        #expect(settings.ignoredUnusedImages == ["temp1"])
+        #expect(settings.ignoredUndefinedImages == ["temp2"])
+        #expect(settings.rastorExtensions == Set<String>(["png", "jpg", "jpeg", "tiff"].map{$0.uppercased()}))
+        #expect(settings.vectorExtensions == Set<String>(["pdf", "svg", "vector"].map{$0.uppercased()}))
+        #expect(settings.sourcesExtensions == Set<String>(["swift", "mm", "c"].map{$0.uppercased()}))
+        #expect(settings.resourcesExtensions == Set<String>(["swift", "storyboard", "xib"].map{$0.uppercased()}))
         #expect(settings.isAllFilesErrorShowing == false)
         #expect(settings.maxVectorFileSize == 30000)
         #expect(settings.maxVectorImageSize == CGSize(width: 100, height: 100))
