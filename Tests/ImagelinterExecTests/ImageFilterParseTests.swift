d3e0245542cf4f50f968af7c50843a24c2bae678
Sergey Balalaev <sof.bix@mail.ru>
2025-12-26 05:27:29 +0400

    extends test with complex parsing filter + add size parsing

--- Tests/ImagelinterExecTests/ImageFilterParseTests.swift
+++ Tests/ImagelinterExecTests/ImageFilterParseTests.swift
@@ -30,21 +30,58 @@ struct ImageFilterParseTests {
         #expect(sizeCondition.size[0].comparison == comparison)
     }
 
-    @Test
-    func complex() throws {
-        let pattern = "vector, >100kpx , <10Mb)"
+    struct StaticSizeTest: TestTrait {
+        let pattern: String
+        let value: Int64
+        let messure: ImageFilter.Messure
+    }
+    @Test(arguments: [
+          StaticSizeTest(pattern: " = 10px", value: Int64(10), messure: .pixels),
+          StaticSizeTest(pattern: "=20kpx", value: Int64(20_000), messure: .pixels),
+          StaticSizeTest(pattern: "=0.3kpx", value: Int64(300), messure: .pixels),
+          StaticSizeTest(pattern: "=0.1Mpx", value: Int64(100_000), messure: .pixels),
+          StaticSizeTest(pattern: "=2000mpx", value: Int64(2_000_000_000), messure: .pixels),
+          StaticSizeTest(pattern: "= 1b", value: Int64(1), messure: .bytes),
+          StaticSizeTest(pattern: "= 20kb", value: Int64(20 * 1024), messure: .bytes),
+          StaticSizeTest(pattern: "= 0.5Kb", value: Int64(0.5 * 1024), messure: .bytes),
+          StaticSizeTest(pattern: "=100Mb", value: Int64(100 * 1024 * 1024), messure: .bytes),
+          StaticSizeTest(pattern: "=0.001Mb", value: Int64(0.001 * 1024 * 1024), messure: .bytes)
+    ])
+    func size(param: StaticSizeTest) throws {
+        let filter = try #require(ImageFilter(param.pattern))
+        #expect(filter.andConditions.count == 1)
+        let orCondition = try #require(filter.andConditions.first as? ImageFilter.OrCondition)
+        let sizeCondition = try #require(orCondition.orConditions.first as? ImageFilter.SizeCondition)
+        #expect(sizeCondition.size[0].comparison == ImageFilter.Comparison.equal)
+        #expect(sizeCondition.size[0].value == param.value)
+        #expect(sizeCondition.size[0].messure == param.messure)
+    }
+
+    @Test(arguments: zip(
+        ["vector, >100kpx , <10Mb",
+         ">100kpx , <10Mb, vector",
+         ">100kpx , vector, <10Mb",
+         "vector, <10Mb, >100kpx"
+        ],
+        [[0, 1, 2],
+         [2, 0, 1],
+         [1, 0, 2],
+         [0, 2, 1]
+        ]
+    ))
+    func complex(pattern: String, order: [Int]) throws {
         let filter = try #require(ImageFilter(pattern))
         #expect(filter.andConditions.count == 3)
-        let orCondition1 = try #require(filter.andConditions[0] as? ImageFilter.OrCondition)
+        let orCondition1 = try #require(filter.andConditions[order[0]] as? ImageFilter.OrCondition)
         let typeCondition = try #require(orCondition1.orConditions.first as? ImageFilter.ImageTypeCondition)
         #expect(typeCondition.types.count == 1)
         #expect(typeCondition.types.contains(.vector) == true)
-        let orCondition2 = try #require(filter.andConditions[1] as? ImageFilter.OrCondition)
+        let orCondition2 = try #require(filter.andConditions[order[1]] as? ImageFilter.OrCondition)
         let sizeCondition1 = try #require(orCondition2.orConditions.first as? ImageFilter.SizeCondition)
         #expect(sizeCondition1.size[0].comparison == ImageFilter.Comparison.greater)
         #expect(sizeCondition1.size[0].value == 100_000)
         #expect(sizeCondition1.size[0].messure == .pixels)
-        let orCondition3 = try #require(filter.andConditions[2] as? ImageFilter.OrCondition)
+        let orCondition3 = try #require(filter.andConditions[order[2]] as? ImageFilter.OrCondition)
         let sizeCondition2 = try #require(orCondition3.orConditions.first as? ImageFilter.SizeCondition)
         #expect(sizeCondition2.size[0].comparison == ImageFilter.Comparison.less)
         #expect(sizeCondition2.size[0].value == 10 * 1024 * 1024)
